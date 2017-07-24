a5d75425cb5d9c8591c8dda50a726940839dd115
YuriiS-68 <yurii_s@bigmir.net>
2017-07-24 14:32:46 +0300

    add dz17

---
+++ src/dz_lesson17/Solution.java
@@ -0,0 +1,61 @@
+package dz_lesson17;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str = "The has !been divided abs on98 the issue, with   moderates5789 concerned @about the on the most vulnerable.";
+
+        System.out.println(countWords(str));
+        //System.out.println(maxWord(str));
+        //System.out.println(minWord(str));
+    }
+
+    public static int countWords(String input){
+        //разбить строку на массив слов по пробелу
+
+        int count = 0;
+        String[] words = input.split("\\s+");
+
+        //посчитать слова, в которых содержатся только буквы
+        for (String word : words) {
+            if (isValidWord(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isValidWord(String word) {
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (!Character.isLetter(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+   /* public static String maxWord(String input){
+        String[] words = input.split("\\s+");
+
+        String max = words[0];
+        for (String word : words) {
+            if (word.length() > max.length()){
+                max = word;
+            }
+        }
+        return max;
+    }
+
+    public static String minWord(String input){
+        String[] words = input.split("\\s+");
+
+        String min = words[0];
+        for (String word : words) {
+            if (word.length() < min.length()){
+                min = word;
+            }
+        }
+        return min;
+    }*/
+}
+
