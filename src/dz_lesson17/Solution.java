ddf201f6d2a46012d14861d323d6e15e5686cb7c
YuriiS-68 <yurii_s@bigmir.net>
2017-07-26 15:46:43 +0300

    add dz17 last change today

--- src/dz_lesson17/Solution.java
+++ src/dz_lesson17/Solution.java
@@ -3,8 +3,17 @@ package dz_lesson17;
 public class Solution {
     public static void main(String[] args) {
         String str = "The has !been divided abs on98 the issue, with   moderates5789 concerned @about the on the most vulnerable.";
+        String str1 = "End";
+        String str2 = null;
 
         System.out.println(countWords(str));
+        System.out.println(countWords(str1));
+
+        System.out.println("Max word: " + maxWord(str));
+        System.out.println("Min word: " + minWord(str));
+
+        System.out.println("Max word: " + maxWord(str2));
+        System.out.println("Min word: " + minWord(str2));
 
     }
 
@@ -31,6 +40,62 @@ public class Solution {
             }
         }
         return !word.trim().isEmpty();
+    }
+
+    public static String maxWord(String input){
+
+        if (input == null){
+            return null;
+        }
+
+        String[] words = input.split(" ");
+
+        String max = "";
+        for (String word : words) {
+            if (isValidWord(word)) {
+                max = word;
+                break;
+            }
+        }
+
+        if (max.equals("")){
+            return null;
+        }
+
+        for (String word : words) {
+            if (isValidWord(word) && word.length() > max.length()) {
+                max = word;
+            }
+        }
+        return max;
+    }
+
+    public static String minWord(String input){
+
+        if (input == null){
+            return null;
+        }
+
+        String[] words = input.split(" ");
+
+        String min = "";
+        for (String word : words) {
+            if (isValidWord(word)){
+                min = word;
+                break;
+            }
+        }
+
+        if (min.equals("")){
+            return null;
+        }
+
+        for (String word : words) {
+            if (isValidWord(word) && word.length() < min.length()){
+                min = word;
+            }
+        }
+        return min;
     }
 }
 
