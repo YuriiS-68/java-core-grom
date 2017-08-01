73c87df1f7f4bf24cc709738e87082bbd99a6b4a
YuriiS-68 <yurii_s@bigmir.net>
2017-08-01 19:30:06 +0300

    add dz18 second variant

--- src/dz_lesson18/Solution1.java
+++ src/dz_lesson18/Solution1.java
@@ -2,7 +2,7 @@ package dz_lesson18;
 
 import java.util.Arrays;
 
-public class Solution {
+public class Solution1 {
     public static void main(String[] args) {
         String str = "12,65 pkmvb 35 74 12.0 87,9 200 ";
 
@@ -12,8 +12,8 @@ public class Solution {
 
     //строка может быть "12 34kv i89k dlx77 34,5 fkgjhvn 19.7"
     //разделить строку на массив слов по пробелу
+    //определить длину нового массива цифр
     //для каждого не целого числа вывести в консоль надпись
-
 
     public static int[] findNumbers(String text){
         String[] words = text.trim().split(" ");
@@ -21,13 +21,22 @@ public class Solution {
         int[] numbers = new int[words.length];
         int index = 0;
         for (int i = 0; i < words.length; i++) {
-            try {
+            if (checkDigit(words[i])) {
                 numbers[index] = Integer.valueOf(words[i]);
                 index++;
-            } catch (Exception e){
-                System.out.println("not a number");
             }
+            else System.out.println("not a number");
         }
         return numbers;
+    }
+
+    public static boolean checkDigit(String word){
+        char[] chars = word.trim().toCharArray();
+        for (char ch : chars) {
+            if (!Character.isDigit(ch)){
+                return false;
+            }
+        }
+        return true;
     }
 }
