9ef1b003c4b41db30004569fd354cfbfd0224147
YuriiS-68 <yurii_s@bigmir.net>
2017-09-26 18:13:20 +0300

    add lesson28 and dz_lesson28 path 1

---
+++ src/dz_lesson28/Capability.java
@@ -0,0 +1,55 @@
+package dz_lesson28;
+
+import java.util.Date;
+
+public class Capability implements Comparable<Capability>{
+    private long id;
+    private String channelName;
+    private String fingerprint;
+    private boolean isActive;
+    private Date dateCreated;
+
+    public Capability(long id, String channelName, String fingerprint, boolean isActive, Date dateCreated) {
+        this.id = id;
+        this.channelName = channelName;
+        this.fingerprint = fingerprint;
+        this.isActive = isActive;
+        this.dateCreated = dateCreated;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public String getFingerprint() {
+        return fingerprint;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    @Override
+    public int compareTo(Capability o) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Capability{" +
+                "id=" + id +
+                ", channelName='" + channelName + '\'' +
+                ", fingerprint='" + fingerprint + '\'' +
+                ", isActive=" + isActive +
+                ", dateCreated=" + dateCreated +
+                '}';
+    }
+}
---
+++ src/dz_lesson28/Demo.java
@@ -0,0 +1,29 @@
+package dz_lesson28;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Demo {
+    public static void main(String[] args) throws Exception{
+        ArrayList<Capability> capabilities = new ArrayList<>();
+
+        Capability capability1 = new Capability(1001, "test", "1111", false, new Date());
+        Thread.sleep(1000);
+        Capability capability2 = new Capability(1002, "atest", "1111", false, new Date());
+        Thread.sleep(1000);
+        Capability capability3 = new Capability(1003, "test", "1111", true, new Date());
+        Thread.sleep(1000);
+        Capability capability4 = new Capability(1004, "test", "1111", true, new Date());
+
+        capabilities.add(capability3);
+        capabilities.add(capability1);
+        capabilities.add(capability4);
+        capabilities.add(capability2);
+
+        System.out.println(capabilities);
+
+        capabilities.sort(new FullComparator());
+
+        System.out.println(capabilities);
+    }
+}
