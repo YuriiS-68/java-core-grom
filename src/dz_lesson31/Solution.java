e72221ef38ba5b1c291cd97d05d342c14fb00d31
YuriiS-68 <yurii_s@bigmir.net>
2017-10-31 15:14:33 +0200

    changes to lesson31 home work

--- src/dz_lesson31/Solution.java
+++ src/dz_lesson31/Solution.java
@@ -1,36 +1,55 @@
 package dz_lesson31;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception{
         //иду циклом по строке
         //беру символ, проверяю буква ли это
         //если ключ с таким символом есть, то заношу его в мапу и считаю сколько раз он повторяется
-        //если нет заношу в мапу и ставлю 1
-        String text = "I need you.";
-        String text2 = "The urn was then carried for several rounds around the cremation site, for the last leg of the procession.";
+        //если нету заношу в мапу и ставлю 1
+        String text = "Aaaaaaa aaa mmmmm";
+        String text2 = "the the the the the the tg";
         System.out.println(countSymbols(text));
         System.out.println(words(text2));
-
     }
 
     public static Map<Character, Integer> countSymbols(String text)throws Exception{
+        //сделать отсортированный список
+        //посчитать количество повторений каждого символа и занести в переменную
+        //результат добавить в мапу
         if (text == null)
             throw new Exception("No text");
 
         Map<Character, Integer> map = new HashMap<>();
+
+        ArrayList<Character> chars = new ArrayList<>();
         for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
+            char c = text.toLowerCase().charAt(i);
             if (Character.isLetter(c)){
-                if (map.containsKey(c)){
-                    map.put(c, map.get(c) + 1);
-                }
-                else map.put(c, 1);
+                chars.add(c);
             }
         }
+        Collections.sort(chars);
+
+        //надо взять первый элемент массива, пройти им по всему списку и если повторяется то плюсовать счетчик
+        //если не повторился то счетчик равен 1
+        Character c = chars.get(0);
+        int count = 1;
+        for (int i = 1; i < chars.size(); i++) {
+            // проверяю текущий символ
+            if (chars.get(i).equals(c)) {
+                // символ тот же - счетчик увеличиваю
+                count++;
+            } else {
+                // другой символ, добавляю предыдущий и запоминаю новый
+                map.put(c, count);
+                c = chars.get(i);
+                count = 1;
+            }
+        }
+        map.put(c, count);
+
         return map;
     }
 
@@ -44,19 +63,26 @@ public class Solution {
         ArrayList<String> words = new ArrayList<>();
         String[] str = text.split("\\W+");
         for (String el : str){
-            if (el != null && isValidWord(el) && el.length() >= 2){
+            if (el != null && isValidWord(el) && el.length() > 1){
                 words.add(el);
             }
         }
+        Collections.sort(words);
 
-        String word;
+        String word = words.get(0);
+        int count = 0;
         for (int i = 0; i < words.size(); i++) {
-            word = words.get(i);
-            if (map.containsKey(word)){
-                map.put(word, map.get(word) + 1);
+            if (words.get(i).equals(word)){
+                count++;
             }
-            else map.put(word, 1);
+            else {
+                map.put(word, count);
+                word = words.get(i);
+                count = 1;
+            }
         }
+        map.put(word, count);
+
         return map;
     }
 
